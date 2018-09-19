ae8c92d6aa071511c5582cd83594122ed5a24c36
Susan Buck <susan@susanbuck.net>
2018-09-19 10:41:31 -0400

    Status of foobooks0 as of Week 3, Part 4 video

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require('includes/helpers.php');
+require('includes/logic.php');
+?>
+
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+
+    <title>Foobooks0</title>
+    <meta charset='utf-8'>
+
+</head>
+<body>
+
+<?php foreach ($books as $title => $book): ?>
+    <div class='book'>
+        <?= $title ?> by <?= $book['author'] ?>
+        <img src='<?= $book['cover_url'] ?>' alt='Cover photo for the book <?= $title ?>'>
+    </div>
+<?php endforeach ?>
+
+</body>
+</html>
